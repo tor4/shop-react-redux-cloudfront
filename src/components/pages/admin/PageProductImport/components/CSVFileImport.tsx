1f1091fd1a47348c309904a767d8f1d9cd2ca026
Natalia Zarubina <natalia_zarubina@epam.com>
2026-05-19 10:38:00 +0200

    Add authorization token handling for file upload

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -10,17 +10,20 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
+  const [error, setError] = React.useState<string>();
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
       const file = files[0];
       setFile(file);
+      setError(undefined);
     }
   };
 
   const removeFile = () => {
     setFile(undefined);
+    setError(undefined);
   };
 
   const uploadFile = async () => {
@@ -29,23 +32,42 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       return;
     }
 
-    // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        fileName: encodeURIComponent(file.name),
-      },
-    });
-    console.log("File to upload: ", file.name);
-    console.log("Uploading to: ", response.data);
-    const result = await fetch(response.data.signedUrl, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+    const authToken = localStorage.getItem("authorization_token");
+    if (!authToken) {
+      console.error("No auth token found");
+      return;
+    }
+    try {
+      // Get the presigned URL
+      const response = await axios({
+        method: "GET",
+        url,
+        headers: {
+          Authorization: `Basic ${authToken}`,
+        },
+        params: {
+          fileName: encodeURIComponent(file.name),
+        },
+      });
+      console.log("File to upload: ", file.name);
+      console.log("Uploading to: ", response.data);
+      const result = await fetch(response.data.signedUrl, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+      setFile(undefined);
+    } catch (error) {
+      console.error("Error uploading file: ", error);
+
+      if (error instanceof Error) {
+        setError("Error uploading file: " + error.message);
+      } else {
+        setError("Error uploading file");
+      }
+    }
   };
+
   return (
     <Box>
       <Typography variant="h6" gutterBottom>
@@ -58,6 +80,11 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
           <button onClick={removeFile}>Remove file</button>
           <button onClick={uploadFile}>Upload file</button>
         </div>
+      )}
+      {error && file && (
+        <Typography variant="body2" color="error" gutterBottom>
+          {error}
+        </Typography>
       )}
     </Box>
   );
